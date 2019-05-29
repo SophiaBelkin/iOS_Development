370360fe3f655a13a818ed275e4182fda6076274
Sophia Lu <sophia.lu@belkin.com>
2019-05-28 21:30:00 -0700

    added todo list project

---
+++ 100_days_swift/GroceryList12/GroceryList12/Model.swift
@@ -0,0 +1,17 @@
+//
+//  Model.swift
+//  GroceryList12
+//
+//  Created by Sophia Lu on 5/28/19.
+//  Copyright © 2019 Sophia Lu. All rights reserved.
+//
+
+import Foundation
+
+var model = Model()
+
+class Model {
+    var data = ["Eggs", "Tomato", "Apples", "Oranges", "Milk"]
+    var cloudData = ["Ice cream", "Melon", "Eggs", "Avocado", "Potatos", "Tomato", "Apples", "Oranges", "Milk"]
+    
+}
